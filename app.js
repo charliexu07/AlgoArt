51467470df9b4d55fa8da860e9009c51a577090c
Jiachen Xu <xujiachen1999@outlook.com>
2020-10-03 20:22:38 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// console.log('Here\'s a hidden message');
+
+// let today = new Date();
+// let formatDate = today.toDateString();
+// let selectElement = document.getElementById('date');
+// selectElement.innerHTML = formatDate;
+
+const realFileBtn = document.getElementById("real-file");
+const customBtn = document.getElementById("custom-button");
+const customTxt = document.getElementById("custom-text");
+
+customBtn.addEventListener("click", function() {
+  realFileBtn.click();
+});
+
+realFileBtn.addEventListener("change", function() {
+  if (realFileBtn.value) {
+    customTxt.innerHTML = realFileBtn.value.match(
+      /[\/\\]([\w\d\s\.\-\(\)]+)$/
+    )[1];
+  } else {
+    customTxt.innerHTML = "No file chosen, yet.";
+  }
+});
